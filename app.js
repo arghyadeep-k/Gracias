2f763c3db2ceeccedc9f9e6b51c902091b1ee666
Arghyadeep <arghyadeep.k@gmail.com>
2018-08-17 15:52:33 +0000

    Minor fixes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var config = require('./config.js');
 var twitter = new Twitter(config);
 var stream = twitter.stream('user');
 
+var myScreenName = 'arghyadeep_k';
 console.log('Starting program. Monitoring started.')
 
 stream.on('follow',followed);
@@ -11,12 +12,18 @@ stream.on('follow',followed);
 function followed(event)
 {
     console.log('Starting Followed event');   
+    
     var name = event.source.name; 
     var screenName = event.source.screen_name;
 
-    console.log(name+' (@'+screenName+') followed you.');
-
-    tweetNow('@'+screenName+', thanks for following me.');
+    //Checking if you followed or you were followed
+    if(screenName!=myScreenName)
+    {
+        console.log(name+' (@'+screenName+') followed you.');
+        tweetNow('@'+screenName+', thanks for following me.');
+    }
+    else
+        console.log('You followed ' + event.target.name +'(@'+ event.target.screen_name + ')');        
 }
 
 //Post tweet
@@ -27,7 +34,8 @@ function tweetNow(tweetMsg)
     }
     twitter.post('statuses/update', tweet, function(err, data, response) {
         if(err){
-          console.log("Error in posting tweet");
+          console.log("Error in posting tweet.");
+          console.log(err);
         }
         else{
           console.log("Tweet posted successfully");
